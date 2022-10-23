0f99e87f2d356ece96924b7021f3afadd14cf32e
An1k11 <112762253+An1k11@users.noreply.github.com>
2022-10-24 02:40:18 +0300

    browser console Rock Paper Scissors game

---
+++ script.js
@@ -0,0 +1,63 @@
+function getComputerChoice() {
+	let max = 3;
+	return Math.floor(Math.random() * max);
+}
+
+function defineUserInput() {
+	let userInput = prompt("Choose: Rock, Paper, or Scissors!");
+	if (userInput.toUpperCase() == "ROCK") {
+		return 0;
+	}
+	else if (userInput.toUpperCase() == "PAPER") {
+		return 1;
+	}
+	else if (userInput.toUpperCase() == "SCISSORS") {
+		return 2;
+	}
+	else {
+		alert("Please, choose correctly!");
+		return defineUserInput();
+	}
+}
+
+function conditions(player, computer) {
+	if (player == computer) {
+		return 2;
+	}
+	else if (player == 0 && computer == 1) {
+		return 0;
+	}
+	else if (player == 0 && computer == 2) {
+		return 1;
+	}
+	else if (player == 1 && computer == 0) {
+		return 1;
+	}
+	else if (player == 1 && computer == 2) {
+		return 0;
+	}
+	else if (player == 2 && computer == 0) {
+		return 0;
+	}
+	else if (player == 2 && computer == 1) {
+		return 1;
+	}
+}
+
+console.log("Rock - 0; Paper - 1; Scissors - 2");
+
+let computer = getComputerChoice();
+let player = defineUserInput();
+
+console.log(`Player - ${player}`);
+console.log(`Computer - ${computer}`);
+
+if (conditions(player, computer) == 2) {
+	console.log("Tie!");
+}
+else if (conditions(player, computer) == 1) {
+	console.log("Win!");
+}
+else if (conditions(player, computer) == 0) {
+	console.log("Lose!");
+}
